be12bf514e71fe3594b0656f9bb5f1816784f380
Silvan Struebi <silvan.struebi@gmail.com>
2019-09-29 11:48:34 +0200

    roll back release 2.13.3
    Signed-off-by: Silvan Struebi <silvan.struebi@gmail.com>

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -3,7 +3,7 @@ importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js')
 class MasterServiceWorker {
     constructor() {
         this.name = 'ServiceWorker'
-        this.version = 'v17'
+        this.version = 'v18'
         this.precache = [
             './',
             './index.html',
